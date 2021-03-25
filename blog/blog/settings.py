4d47c72f58d482c03b8e54326b8bc1a99b4de975
niufuquan1 <nfuquan@gmail.com>
2021-03-25 22:44:28 +0800

    配置修改，将数据库源修改为mysql

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':3306,
+        'USER':'blog_admin',
+        'PASSWORD':'123456',
+        'NAME':'blog'
     }
 }
 
