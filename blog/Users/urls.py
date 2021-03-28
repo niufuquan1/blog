d7a980579476e4e0d322967eaee37f1385f123ff
niufuquan1 <nfuquan@gmail.com>
2021-03-28 13:35:50 +0800

    忘记密码页面展示功能实现

--- blog/Users/urls.py
+++ blog/Users/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from Users.views import RegisterView, ImageCodeView, SmsCodeView, LoginView, LogoutView
+from Users.views import RegisterView, ImageCodeView, SmsCodeView, LoginView, LogoutView, ForgetPasswordView
 
 urlpatterns = [
-    path('register/',RegisterView.as_view(),name='register'),
-    #图片验证码
-    path('imagecode/',ImageCodeView.as_view(),name='imagecode'),
-    #短信验证码
-    path('smscode/',SmsCodeView.as_view(),name='smscode'),
-
-    path('login/',LoginView.as_view(),name='login'),
-
-    path('logout/',LogoutView.as_view(),name='logout'),
-
+    path('register/', RegisterView.as_view(), name='register'),
+    # 图片验证码
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
+    # 短信验证码
+    path('smscode/', SmsCodeView.as_view(), name='smscode'),
+    # 登录
+    path('login/', LoginView.as_view(), name='login'),
+    # 退出
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # 忘记密码
+    path('forgetpassword/', ForgetPasswordView.as_view(), name='forgetpassword')
 ]
--- blog/Users/views.py
+++ blog/Users/views.py
@@ -237,3 +237,8 @@ class LogoutView(View):
         response.delete_cookie('is_login')
 
         return response
+
+class ForgetPasswordView(View):
+
+    def get(self,request):
+        return render(request,'forget_password.html')
